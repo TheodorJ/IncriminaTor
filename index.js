8e070593a0d92fc527579fed8e133aa5f73bdda1
TheodorJ <teddyjohansson@live.com>
2018-10-23 10:33:19 -0400

    Added app information in .js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 new Vue({
-    el: '#example',
-    data: { hello: 'Hello World!' }
+    el: '#app',
+    data: {
+      ip: '',
+      date: '',
+      hello: ''
+   },
+   methods: {
+     diveIn: function(e) {
+       if(this.ip && this.date) {
+         this.hello = "Hello world!";
+         // We have a valid ip/date pair! We now proceed to
+         // look up data for these values.
+       }
+       else {
+         this.hello = "Invalid info :()";
+       }
+     }
+   }
 })
